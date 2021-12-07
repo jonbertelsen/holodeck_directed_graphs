5412581f43b87d865008f1fb2e459105d2fd57bb
Jon Bertelsen <jobe@cphbusiness.dk>
2021-12-07 23:06:35 +0100

    first commit

---
+++ Assets/Graph.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+
+  public TextAsset file;
+  public GameObject nodepf;
+  public GameObject edgepf; 
+  public float width;
+  public float length;
+  public float height;
+
+	void Start()
+    {      
+      if (file==null){
+			// instantiate A, B, C, D, E
+	GameObject A = Instantiate(nodepf, new Vector3(Random.Range(-width/2, width/2), Random.Range(-length/2, length/2), Random.Range(-height/2, height/2)), Quaternion.identity, transform);
+	GameObject B = Instantiate(nodepf, new Vector3(Random.Range(-width/2, width/2), Random.Range(-length/2, length/2), Random.Range(-height/2, height/2)), Quaternion.identity, transform);
+	GameObject C = Instantiate(nodepf, new Vector3(Random.Range(-width/2, width/2), Random.Range(-length/2, length/2), Random.Range(-height/2, height/2)), Quaternion.identity, transform);
+	GameObject D = Instantiate(nodepf, new Vector3(Random.Range(-width/2, width/2), Random.Range(-length/2, length/2), Random.Range(-height/2, height/2)), Quaternion.identity, transform);
+	GameObject E = Instantiate(nodepf, new Vector3(Random.Range(-width/2, width/2), Random.Range(-length/2, length/2), Random.Range(-height/2, height/2)), Quaternion.identity, transform);
+
+	// change name
+	
+	A.name = "node A"; 
+	B.name = "node B";
+	C.name = "node C";
+	D.name = "node D";
+	E.name = "node E";
+
+	// get script instances
+
+
+	Node AS = A.GetComponent<Node>();
+	Node BS = B.GetComponent<Node>();
+	Node CS = C.GetComponent<Node>();
+	Node DS = D.GetComponent<Node>();
+	Node ES = E.GetComponent<Node>();
+
+	// add edges
+
+	AS.AddFixedJoint();
+	AS.SetEdgePrefab(edgepf); AS.AddEdge(BS);
+	AS.AddEdge(CS);
+	CS.SetEdgePrefab(edgepf); CS.AddEdge(DS); 
+	DS.SetEdgePrefab(edgepf); DS.AddEdge(ES);
+	DS.AddEdge(AS); 
+      } else {	
+	LoadGMLFromFile(file);
+      }      
+    }
+
+    void Update(){}
+
+    void LoadGMLFromFile(TextAsset f){
+	  bool firstNode = true;
+		Vector3 nodeOrigin;
+
+	  string[] lines = f.text.Split('\n');
+      int currentobject = -1; // 0 = graph, 1 = node, 2 = edge
+      int stage = -1; // 0 waiting to open, 1 = waiting for attribute, 2 = waiting for id, 3 = waiting for label, 4 = waiting for source, 5 = waiting for target
+
+	Node n = null;
+      Dictionary<string,Node> nodes = new Dictionary<string,Node>();
+      foreach (string line in lines){
+	string l = line.Trim();
+	string [] words = l.Split(' ');
+	foreach(string word in words){
+	  if (word == "graph" && stage == -1) {
+	    currentobject = 0;
+	  }
+	  if (word == "node" && stage == -1) {
+	    currentobject = 1;
+	    stage = 0;	    
+	  }
+	  if (word == "edge" && stage == -1) {
+	    currentobject = 2;
+	    stage = 0;	    
+	  }
+	  if (word == "[" && stage == 0 && currentobject == 2){
+	    stage = 1;
+	  }
+	  if (word == "[" && stage == 0 && currentobject == 1){
+	    stage = 1;
+		if (firstNode)
+		{
+			nodeOrigin = new Vector3(0,0,0);
+		}
+		else {
+			nodeOrigin = new Vector3(Random.Range(-width / 2, width / 2), Random.Range(-length / 2, length / 2), Random.Range(-height / 2, height / 2));
+		}
+
+	    GameObject go = Instantiate(nodepf, nodeOrigin, Quaternion.identity);
+	    n = go.GetComponent<Node>();
+	    n.transform.parent = transform;
+	    n.SetEdgePrefab(edgepf);
+		if (firstNode) {
+			n.AddFixedJoint();
+			firstNode = false;
+		}
+	    continue;
+	  }
+	  if (word == "]"){
+	    stage = -1;
+	  }
+	  if (word == "id" && stage == 1 && currentobject == 1){
+	    stage = 2;
+	    continue;
+	  }
+	  if (word == "label" && stage == 1 && currentobject == 1){
+	    stage = 3;
+	    continue;
+	  }
+	  if (stage == 2){
+	    nodes.Add(word, n);
+	    stage = 1;
+	    break;
+	  }
+	  if (stage == 3){
+	    n.name = word;
+	    stage = 1;
+	    break;
+	  }
+	  if (word == "source" && stage == 1 && currentobject == 2){
+	    stage = 4;
+	    continue;
+	  }
+	  if (word == "target" && stage == 1 && currentobject == 2){
+	    stage = 5;
+	    continue;
+	  }
+	  if (stage == 4){
+	    n = nodes[word];
+	    stage = 1;
+	    break;
+	  }
+	  if (stage == 5){
+	    n.AddEdge(nodes[word]);
+	    stage = 1;
+	    break;
+	  }
+	}
+      }
+    }
+    
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Node : MonoBehaviour
+{
+
+  GameObject epf;
+
+  List<GameObject>  edges  = new List<GameObject> ();
+  List<SpringJoint> joints = new List<SpringJoint>();  
+  
+  void Start(){
+    transform.GetChild(0).GetComponent<TextMeshPro>().SetText(name);
+  }
+  
+  void Update(){    
+    int i = 0;
+    foreach (GameObject edge in edges){
+      edge.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+      SpringJoint sj = joints[i];
+      GameObject target = sj.connectedBody.gameObject;
+      edge.transform.LookAt(target.transform);
+      Vector3 ls = edge.transform.localScale;
+      ls.z = Vector3.Distance(transform.position, target.transform.position);
+      edge.transform.localScale = ls;
+      edge.transform.position = new Vector3((transform.position.x+target.transform.position.x)/2,
+					    (transform.position.y+target.transform.position.y)/2,
+					    (transform.position.z+target.transform.position.z)/2);
+      i++;
+    }
+  }
+
+  public void SetEdgePrefab(GameObject epf){
+    this.epf = epf;
+  }
+  
+  public void AddEdge(Node n){
+    SpringJoint sj = gameObject.AddComponent<SpringJoint> ();  
+    sj.autoConfigureConnectedAnchor = false;
+    sj.anchor = new Vector3(0.5f, 0.5f, 0.5f);
+    sj.connectedAnchor = new Vector3(0.5f, 0.5f, 0.5f);    
+    sj.enableCollision = true;
+        sj.spring = 10;
+        sj.damper = 0.2f;
+    sj.connectedBody = n.GetComponent<Rigidbody>();
+    GameObject edge = Instantiate(this.epf, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+    edges.Add(edge);
+    joints.Add(sj);
+  }
+
+    public void AddFixedJoint() {
+
+        FixedJoint fp = gameObject.AddComponent<FixedJoint>();
+
+        fp.autoConfigureConnectedAnchor = false;
+        fp.anchor = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+    
+}
